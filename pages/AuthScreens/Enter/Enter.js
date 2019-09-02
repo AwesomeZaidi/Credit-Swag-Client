c5a2c71e7a72383408f865b7beb8e60c34190bb8
Asim Zaidi <asimzaidih@gmail.com>
2019-09-01 20:51:17 -0500

    added keyboard avoiding view

--- pages/AuthScreens/Enter/Enter.js
+++ pages/AuthScreens/Enter/Enter.js
@@ -8,7 +8,10 @@ import {
     Image,
     ScrollView,
     TextInput,
-    TouchableOpacity
+    TouchableOpacity,
+    KeyboardAvoidingView,
+    TouchableWithoutFeedback,
+    Keyboard
 } from 'react-native';
 
 import React, { Component } from 'react';
@@ -18,15 +21,15 @@ import common from '../../styles/common.style';
 import styles from '../styles/forms.js';
 import { placeholder } from '../../styles/variables';
 
-import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
-import { faArrowRight } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
 import { Notifications } from 'expo';
 import * as Permissions from 'expo-permissions';
 
 // ----------------------------------------------------------------------------------
 // Enter Component Class
 // ----------------------------------------------------------------------------------
-class Enter extends Component {    
+class Enter extends Component {
     
     // ------------------------------------------
     // State
@@ -120,10 +123,9 @@ class Enter extends Component {
             }
             
             // Get the token that uniquely identifies this device
-            let token = await Notifications.getExpoPushTokenAsync();
-
+            let notificationToken = await Notifications.getExpoPushTokenAsync();
             // Step 2 signup api call
-            await this.props.signUp(this.state, token)
+            await this.props.signUp(this.state, notificationToken)
             
             !this.props.error === true
                 ?
@@ -167,124 +169,125 @@ class Enter extends Component {
                 keyboardShouldPersistTaps='handled'
                 contentContainerStyle={styles.wrapper}
             >
-            {/* This View allows the user to toggle the login and logout tabs to show different forms. */}
-                <View style={common.centerVerticalElements}>
-                    <Text
-                        onPress={() =>this.changeFormView('loginPressed')}                        
-                        style={this.state.loginShow === true
-                        ? common.titleActive : common.titleUnactive}>
-                        Login
-                    </Text>
-                    <Text
-                        onPress={() => this.changeFormView('signupPressed')}
-                        style={this.state.signupShow === true  ?
-                        common.titleActive : common.titleUnactive}>
-                        Signup
-                    </Text>
-                </View>
-                    {
-                        (this.state.signupShow === true) ?
-                            // Shows the signup form or login form based on the state that gets updated above or set by default.
-                            <>
-                                <View style={styles.form}>
-                                    <TextInput
-                                        onChangeText={(text) => this.onChangeText(text, 'name')}
-                                        value={this.state.name}
-                                        style={this.state.nameFocused ? [styles.input, styles.inputFieldFocus] : [styles.input, styles.inputFieldBlur]}
-                                        onFocus={ () => this.onFocus('nameFocused') }
-                                        onBlur={ () => this.onBlur('name', 'nameFocused') }
-                                        placeholder='Full Name'
-                                        placeholderTextColor={placeholder}
-                                        autoCapitalize='none'
-                                    />
-                                    <TextInput
-                                        onChangeText={(text) => this.onChangeText(text, 'username')}
-                                        value={this.state.username}
-                                        style={this.state.usernameFocused ? [styles.input, styles.inputFieldFocus] : [styles.input, styles.inputFieldBlur]}
-                                        onFocus={ () => this.onFocus('usernameFocused') }
-                                        onBlur={ () => this.onBlur('username', 'usernameFocused') }
-                                        placeholder='Username'
-                                        autoCapitalize='none'
-                                        placeholderTextColor={placeholder}
-                                    />
-                                    <TextInput
-                                        onChangeText={(text) => this.onChangeText(text, 'email')}
-                                        value={this.state.email}
-                                        style={this.state.emailFocused ? [styles.input, styles.inputFieldFocus] : [styles.input, styles.inputFieldBlur]}
-                                        onFocus={ () => this.onFocus('emailFocused') }
-                                        onBlur={ () => this.onBlur('email', 'emailFocused') }
-                                        placeholder='Email Address'
-                                        placeholderTextColor={placeholder}
-                                        autoCapitalize='none'
-                                        keyboardType='email-address'
-                                    />
-                                    <TextInput
-                                        onChangeText={(text) => this.onChangeText(text, 'password')}
-                                        value={this.state.password}
-                                        style={this.state.passwordFocused ? [styles.input, styles.inputFieldFocus] : [styles.input, styles.inputFieldBlur]}
-                                        onFocus={ () => this.onFocus('passwordFocused') }
-                                        onBlur={ () => this.onBlur('password', 'passwordFocused') }
-                                        placeholder='Password'
-                                        placeholderTextColor={placeholder}
-                                        autoCapitalize = 'none'
-                                        secureTextEntry={true}
-                                    />
-                                    {this.state.showError === true ? <Text style={common.errorMsg}>Something went wrong</Text> : null }
-                                    <TouchableOpacity style={common.iconBtn} onPress={() => this.handleSubmit('signup')}>
-                                        <FontAwesomeIcon
-                                            style={common.icon}
-                                            size={24}
-                                            icon={ faArrowRight } 
+                <KeyboardAvoidingView enabled>
+                {/* This View allows the user to toggle the login and logout tabs to show different forms. */}
+                    <View style={common.centerVerticalElements}>
+                        <Text
+                            onPress={() =>this.changeFormView('loginPressed')}                        
+                            style={this.state.loginShow === true
+                            ? common.titleActive : common.titleUnactive}>
+                            Login
+                        </Text>
+                        <Text
+                            onPress={() => this.changeFormView('signupPressed')}
+                            style={this.state.signupShow === true  ?
+                            common.titleActive : common.titleUnactive}>
+                            Signup
+                        </Text>
+                    </View>
+                        {
+                            (this.state.signupShow === true) ?
+                                // Shows the signup form or login form based on the state that gets updated above or set by default.
+                                <>
+                                    <View style={styles.form}>
+                                        <TextInput
+                                            onChangeText={(text) => this.onChangeText(text, 'name')}
+                                            value={this.state.name}
+                                            style={this.state.nameFocused ? [styles.input, styles.inputFieldFocus] : [styles.input, styles.inputFieldBlur]}
+                                            onFocus={ () => this.onFocus('nameFocused') }
+                                            onBlur={ () => this.onBlur('name', 'nameFocused') }
+                                            placeholder='Full Name'
+                                            placeholderTextColor={placeholder}
+                                            autoCapitalize='none'
                                         />
-                                    </TouchableOpacity>
-                                </View>
-                                <View style={[common.centerVerticalElements, common.mw]}>
-                                    <Text style={[common.text_sm, common.graytxt]}>
-                                    By Signing Up, you Agree to the Terms and Conditions of the app.
-                                    </Text>
-                                </View>
-                            </>
-                        :
-                        <>
-                        <View style={styles.form}>
-                            <TextInput
-                                onChangeText={(text) => this.onChangeText(text, 'email')}
-                                value={this.state.email}
-                                style={this.state.emailFocused ? [styles.input, styles.inputFieldFocus] : [styles.input, styles.inputFieldBlur]}
-                                onFocus={ () => this.onFocus('emailFocused') }
-                                onBlur={ () => this.onBlur('email', 'emailFocused') }
-                                placeholder='Email'
-                                autoCapitalize='none'
-                                placeholderTextColor={placeholder}
-                            />
-                            <TextInput
-                                onChangeText={(text) => this.onChangeText(text, 'password')}
-                                value={this.state.password}
-                                style={this.state.passwordFocused ? [styles.input, styles.inputFieldFocus] : [styles.input, styles.inputFieldBlur]}
-                                onFocus={ () => this.onFocus('passwordFocused') }
-                                onBlur={ () => this.onBlur('password', 'passwordFocused') }
-                                placeholder='Password'
-                                placeholderTextColor={placeholder}
-                                autoCapitalize = 'none'
-                                secureTextEntry={true}
-                            />
-                            {this.state.showError === true ? <Text style={common.errorMsg}>Something went wrong</Text> : null }
-                            <TouchableOpacity style={common.iconBtn} onPress={() => this.handleSubmit('login')}>
-                                <FontAwesomeIcon
-                                    style={common.icon}
-                                    size={24}
-                                    icon={ faArrowRight }
+                                        <TextInput
+                                            onChangeText={(text) => this.onChangeText(text, 'username')}
+                                            value={this.state.username}
+                                            style={this.state.usernameFocused ? [styles.input, styles.inputFieldFocus] : [styles.input, styles.inputFieldBlur]}
+                                            onFocus={ () => this.onFocus('usernameFocused') }
+                                            onBlur={ () => this.onBlur('username', 'usernameFocused') }
+                                            placeholder='Username'
+                                            autoCapitalize='none'
+                                            placeholderTextColor={placeholder}
+                                        />
+                                        <TextInput
+                                            onChangeText={(text) => this.onChangeText(text, 'email')}
+                                            value={this.state.email}
+                                            style={this.state.emailFocused ? [styles.input, styles.inputFieldFocus] : [styles.input, styles.inputFieldBlur]}
+                                            onFocus={ () => this.onFocus('emailFocused') }
+                                            onBlur={ () => this.onBlur('email', 'emailFocused') }
+                                            placeholder='Email Address'
+                                            placeholderTextColor={placeholder}
+                                            autoCapitalize='none'
+                                            keyboardType='email-address'
+                                        />
+                                        <TextInput
+                                            onChangeText={(text) => this.onChangeText(text, 'password')}
+                                            value={this.state.password}
+                                            style={this.state.passwordFocused ? [styles.input, styles.inputFieldFocus] : [styles.input, styles.inputFieldBlur]}
+                                            onFocus={ () => this.onFocus('passwordFocused') }
+                                            onBlur={ () => this.onBlur('password', 'passwordFocused') }
+                                            placeholder='Password'
+                                            placeholderTextColor={placeholder}
+                                            autoCapitalize = 'none'
+                                            secureTextEntry={true}
+                                        />
+                                        {this.state.showError === true ? <Text style={common.errorMsg}>Something went wrong</Text> : null }
+                                        <TouchableOpacity style={common.iconBtn} onPress={() => this.handleSubmit('signup')}>
+                                            <FontAwesomeIcon
+                                                style={common.icon}
+                                                size={24}
+                                                icon={ faArrowRight } 
+                                            />
+                                        </TouchableOpacity>
+                                    </View>
+                                    <View style={[common.centerVerticalElements, common.mw]}>
+                                        <Text style={[common.text_sm, common.graytxt]}>
+                                        By Signing Up, you Agree to the Terms and Conditions of the app.
+                                        </Text>
+                                    </View>
+                                </>
+                            :
+                            <>
+                            <View style={styles.form}>
+                                <TextInput
+                                    onChangeText={(text) => this.onChangeText(text, 'email')}
+                                    value={this.state.email}
+                                    style={this.state.emailFocused ? [styles.input, styles.inputFieldFocus] : [styles.input, styles.inputFieldBlur]}
+                                    onFocus={ () => this.onFocus('emailFocused') }
+                                    onBlur={ () => this.onBlur('email', 'emailFocused') }
+                                    placeholder='Email'
+                                    autoCapitalize='none'
+                                    placeholderTextColor={placeholder}
                                 />
-                            </TouchableOpacity>
-                        </View>
-                        <View style={[common.centerVerticalElements, common.mw]}>
-                            <Text style={[common.text_sm, common.graytxt]}>
-                            Forgot Password.
-                            </Text>
-                        </View>
-                    </>
-                    }
-
+                                <TextInput
+                                    onChangeText={(text) => this.onChangeText(text, 'password')}
+                                    value={this.state.password}
+                                    style={this.state.passwordFocused ? [styles.input, styles.inputFieldFocus] : [styles.input, styles.inputFieldBlur]}
+                                    onFocus={ () => this.onFocus('passwordFocused') }
+                                    onBlur={ () => this.onBlur('password', 'passwordFocused') }
+                                    placeholder='Password'
+                                    placeholderTextColor={placeholder}
+                                    autoCapitalize = 'none'
+                                    secureTextEntry={true}
+                                />
+                                {this.state.showError === true ? <Text style={common.errorMsg}>Something went wrong</Text> : null }
+                                <TouchableOpacity style={common.iconBtn} onPress={() => this.handleSubmit('login')}>
+                                    <FontAwesomeIcon
+                                        style={common.icon}
+                                        size={24}
+                                        icon={ faArrowRight }
+                                    />
+                                </TouchableOpacity>
+                            </View>
+                            <View style={[common.centerVerticalElements, common.mw]}>
+                                <Text style={[common.text_sm, common.graytxt]}>
+                                Forgot Password.
+                                </Text>
+                            </View>
+                        </>
+                        }
+                </KeyboardAvoidingView>
             </ScrollView>
         );
     };
