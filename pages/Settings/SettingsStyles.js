5097bc13eafaa12d10079e81fc354b64fa2d94aa
Asim Zaidi <asimzaidih@gmail.com>
2019-09-01 20:23:17 -0500

    created basic settings styles

---
+++ pages/Settings/SettingsStyles.js
@@ -0,0 +1,20 @@
+import { StyleSheet } from 'react-native';
+// import {
+//     bg, secondaryBg,
+//     primary, secondary,
+//     danger, placeholder,
+//     light, purple,
+//     third, text,
+//     inputbg, green,
+//     labelText,
+//     inputText
+// } from '../styles/variables';
+import {heightPercentageToDP as hp} from 'react-native-responsive-screen';
+
+export default StyleSheet.create({
+    settingsPage: {
+        flex: 1,
+        height: hp('100%'),
+        paddingLeft: 18
+    },
+})
