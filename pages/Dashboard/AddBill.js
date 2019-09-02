d3d54e448a746d3e8c0b1088ba6aae4ae8414a4c
Asim Zaidi <asimzaidih@gmail.com>
2019-09-01 20:48:40 -0500

    addBill date picker fixed

--- pages/Dashboard/AddBill.js
+++ pages/Dashboard/AddBill.js
@@ -5,15 +5,24 @@ import {
     Text,
     TextInput,
     TouchableOpacity,
+    KeyboardAvoidingView,
+    Button,
+    TouchableWithoutFeedback,
+    Keyboard
 } from 'react-native';
+import { addBill } from "../../redux/actions/index";
 import { connect } from "react-redux";
 import common from '../styles/common.style';
-import styles from '../AuthScreens/styles/forms';
-import billStyles from './billStyles';
+import styles from './billStyles';
 
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
 import { Dropdown } from 'react-native-material-dropdown';
-
+// import DatePicker from 'react-native-datepicker';
+// import DatePicker from 'react-native-date-picker';
+import DateTimePicker from "react-native-modal-datetime-picker";
+import moment from 'moment';
+import {widthPercentageToDP as wp} from 'react-native-responsive-screen';
 // To add a bill, you fill out this form with a name amount, date  and recurring value.
 // You can also add a date for when its due
 // When you hit submit, we submit that to save in our database under the user, as a doc ref to their bills.
@@ -24,9 +33,13 @@ class AddBill extends Component {
     state = {
         name:'',
         amount: '',
+        category: '',
         nameFocused: false,
         amountFocused: false,
+        date: moment(new Date()).format("DD/MM/YYYY"),
+        isDateTimePickerVisible: false
     };
+    // date:"2019-09-01"
 
     static navigationOptions = ({ navigation }) => {
         return {
@@ -39,6 +52,23 @@ class AddBill extends Component {
            }
         }
     }
+
+    showDateTimePicker = () => {
+        this.setState({ isDateTimePickerVisible: true });
+      };
+     
+    hideDateTimePicker = (date) => {
+        this.setState({
+            date: date,
+            isDateTimePickerVisible: false
+        });
+    };
+
+    handleDatePicked = date => {
+        console.log("A date has been picked: ", date);
+        // moment(new Date()).format("DD/MM/YYYY"))
+        this.hideDateTimePicker(moment(date).format("MM/DD/YYYY"));
+    };
 
     onChangeText = (text, feildName) => {
         this.setState({
@@ -56,55 +86,90 @@ class AddBill extends Component {
         });
     };
 
-    render() {
+    onBlur = (feildName, type) => {
+        // if the input field is empty, change its focus state to false.
+        !this.state[feildName] ?
+            this.setState({
+                [type]: false
+            })
+        :
+        null
+    };
 
+    handleSubmit = async () => {
+        this.props.addBill(this.state, this.props.user._id);
+        return this.props.navigation.navigate('Dashboard')
+    } 
+
+    render() {
+        // console.log('date', moment(new Date()).format("DD/MM/YYYY"));
         let data = [
-            { value: 'Banana'},
-            { value: 'Mango' },
-            { value: 'Pear'}
+            { value: 'Housing'},
+            { value: 'Travel' },
+            { value: 'Food and Drink' },
+            { value: 'School' },
+            { value: 'Other' },
         ];
 
         return (
-            <View style={common.page}>
-                <Text style={[common.h1_primary, common.center]}>Add a Bill</Text>
-                <View style={common.centerVerticalElements}>
-                    <View style={styles.form}>
-                        <TextInput
-                            onChangeText={(text) => this.onChangeText(text, 'name')}
-                            value={this.state.name}
-                            style={this.state.nameFocused ? [styles.input, styles.inputFieldFocus] : [styles.input, styles.inputFieldBlur]}
-                            onFocus={ () => this.onFocus('nameFocused') }
-                            onBlur={ () => this.onBlur('name', 'nameFocused') }
-                            placeholder='Bill Name'
-                            placeholderTextColor={styles.placeholder}
-                            autoCapitalize='none'
-                        />
-                        <TextInput
-                            onChangeText={(text) => this.onChangeText(text, 'amount')}
-                            value={this.state.username}
-                            style={this.state.amountFocused ? [styles.input, styles.inputFieldFocus] : [styles.input, styles.inputFieldBlur]}
-                            onFocus={ () => this.onFocus('amountFocused') }
-                            onBlur={ () => this.onBlur('amount', 'amountFocused') }
-                            placeholder='Amount'
-                            autoCapitalize='none'
-                            placeholderTextColor={styles.placeholder}
-                        />
-                        <Dropdown
-                            style={billStyles.dropdown}
-                            label='Favorite Fruit'
-                            data={data}
-                        />
-                        {this.state.showError === true ? <Text style={common.errorMsg}>Something went wrong</Text> : null }
-                        <TouchableOpacity style={common.iconBtn} onPress={() => this.handleSubmit()}>
-                            <FontAwesomeIcon
-                                style={common.icon}
-                                size={24}
-                                icon={ faPlus } 
+            <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+                <View style={common.page}>
+                    <Text style={[common.h1_primary, common.center]}>Add a Bill</Text>
+                    <View style={common.centerVerticalElements}>
+                        <View style={styles.form}>
+                            <Dropdown
+                                textColor='white'
+                                // baseColor='white'
+                                selectedItemColor='#000'
+                                style={styles.dropdown}
+                                label='Category'
+                                data={data}
+                                onChangeText={(value) => this.onChangeText(value, 'category')}
                             />
-                        </TouchableOpacity>
+                            <TextInput
+                                onChangeText={(text) => this.onChangeText(text, 'name')}
+                                value={this.state.name}
+                                style={this.state.nameFocused ? [styles.input, styles.inputFieldFocus] : [styles.input, styles.inputFieldBlur]}
+                                onFocus={ () => this.onFocus('nameFocused') }
+                                onBlur={ () => this.onBlur('name', 'nameFocused') }
+                                placeholder='Bill Name'
+                                placeholderTextColor={styles.placeholder}
+                                autoCapitalize='none'
+                            />
+                            <TextInput
+                                keyboardType='numeric'
+                                onChangeText={(text) => this.onChangeText(text, 'amount')}
+                                value={this.state.amount}
+                                style={this.state.amountFocused ? [styles.input, styles.inputFieldFocus] : [styles.input, styles.inputFieldBlur]}
+                                onFocus={ () => this.onFocus('amountFocused') }
+                                onBlur={ () => this.onBlur('amount', 'amountFocused') }
+                                placeholder='Amount'
+                                autoCapitalize='none'
+                                placeholderTextColor={styles.placeholder}
+                            />
+                            <View style={styles.addContainerCenter}>
+                                <Button style={common.btnPrimary} title="Change Date" onPress={this.showDateTimePicker} />
+                                <DateTimePicker
+                                    isVisible={this.state.isDateTimePickerVisible}
+                                    onConfirm={this.handleDatePicked}
+                                    onCancel={this.hideDateTimePicker}
+                                />
+                            </View>
+                            <Text style={styles.dateText}>{String(this.state.date)}</Text>
+                            <View style={styles.addContainerCenter}>
+                                {this.state.showError === true ? <Text style={common.errorMsg}>Something went wrong</Text> : null }
+                                <TouchableOpacity style={common.circle} onPress={() => this.handleSubmit()}>
+                                    <FontAwesomeIcon
+                                        style={common.icon}
+                                        size={24}
+                                        icon={ faPlus } 
+                                    />
+                                </TouchableOpacity>
+                            </View>       
+                        </View>
                     </View>
                 </View>
-            </View>
+            </TouchableWithoutFeedback>
         );
     }
 }
@@ -115,5 +180,10 @@ const mapStateToProps = state => {
     }
 };
 
-export default connect(mapStateToProps, null)(AddBill);
+function mapDispatchToProps() {
+    return {
+        addBill
+    };
+};
 
+export default connect(mapStateToProps, mapDispatchToProps())(AddBill);
