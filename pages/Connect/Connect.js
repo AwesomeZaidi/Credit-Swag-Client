eeac0d45ada904533ed3e49938add458e77e9e44
Asim Zaidi <asimzaidih@gmail.com>
2019-09-12 08:05:29 -0500

    removed commented debugging from connect phase

--- pages/Connect/Connect.js
+++ pages/Connect/Connect.js
@@ -12,7 +12,6 @@ import {
 
 import PlaidAuthenticator from 'react-native-plaid-link';
 import { connectBank } from '../../redux/actions/index';
-// import { logOut, connectBank } from '../../redux/actions/index';
 import common from '../styles/common.style';
 import { connect } from "react-redux";
 
@@ -33,20 +32,9 @@ class Connect extends Component {
         header: null,
     }; 
 
-    // _signOutAsync = async () => {
-    //     this.props.logOut();
-    //     this.props.navigation.navigate('Auth');
-    // };
-
     render() {
         return (
             <>  
-                {/* <Text>Hello  logoutman</Text>
-                <Text>Hello  logoutman</Text>
-                <Text>Hello  logoutman</Text>
-                <Text>Hello  logoutman</Text>
-                <Text>Hello  logoutman</Text>
-                <Button style={common.h1_primary} onPress={() => this._signOutAsync()} title='Logout'/> */}
                 <PlaidAuthenticator
                     onMessage={this._onMessage}
                     publicKey="e7325291c9f6c0bdb72a3829865923"
@@ -76,73 +64,8 @@ const mapStateToProps = state => {
 
 function mapDispatchToProps() {
     return {
-        logOut,
         connectBank,
     };
 };
 
 export default connect(mapStateToProps, mapDispatchToProps())(Connect);
-
-
-// My hook attempt, got fucked.
-// TODO: Refactor into a hook.
-// const Connect = (props) => {
-
-//     console.log('here in Connect');
-
-//     console.log('props.error:', props.error);
-    
-//     const [error, setError] = useState(props.error);
-    
-//     useEffect(() => {
-//         console.log('in effect, setting error error prop...', props.error);
-//         setError(props.error)
-//     }, []);
-//     console.log('state error:',error);
-
-
-//     _onMessage = async (data) => {   
-//         console.log('in func');
-         
-//         if (data && data.metadata && 'public_token' in data.metadata) {
-//             await props.connectBank(props.user._id, data.metadata.public_token);
-//             console.log('hit connect');
-//             console.log('new error:', error);
-//             if (error) {
-//                 console.log('in if error:', error);
-                
-//                 return (
-//                     <>
-//                         <Text style={common.errorMsg}>{error}</Text>
-//                     </>
-//                 )
-//             } else {
-//                 props.navigation.navigate('App');
-//             }
-//         }
-//     }
-
-//     if (error) {
-//         return (
-//             // Create Error Message component/page.
-//             <Text style={common.errorMsg}>{error}</Text>
-//         )
-//     } else {
-//         return (
-//             <>  
-//             <PlaidAuthenticator
-//                 onMessage={() => _onMessage()}
-//                 publicKey="e7325291c9f6c0bdb72a3829865923"
-//                 env="sandbox"
-//                 product="auth,transactions"
-//                 clientName="Credit Swag"
-//                 selectAccount={false}
-//             />
-//             </>
-//         )
-//     }
-// };
-
-// Connect.navigationOptions = {
-//     header: null,
-// };
