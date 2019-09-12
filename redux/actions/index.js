75e21b59dc22698bd2cbf1c01caee2a295481388
Asim Zaidi <asimzaidih@gmail.com>
2019-09-12 08:38:42 -0500

    Updated redux

--- redux/actions/index.js
+++ redux/actions/index.js
@@ -11,40 +11,54 @@ import {
   
   GET_BALANCE_AND_TRANSACTIONS, GET_BALANCE_GRAPH_DATA,
   
-  HANDLE_ERROR, LOAD_DATA,
+  HANDLE_ERROR, CLEAR_ERROR, LOAD_DATA, SET_ERROR,
   
   UPDATE_BIG_TRANSACTION_NOTIFICATION,
   UPDATE_MINIMUM_BALANCE_NOTIFICATION,
   UPDATE_OVERDRAFT_NOTIFICATION,
   
-  ADD_BILL, ADD_GOAL,
-
-  GET_SAVING_GOALS
+  ADD_BILL, ADD_GOAL_UPDATE_USER, FETCH_GOAL_UPDATE_USER,
+  GET_USER
 } from '../constants/action-types';
 
 // const baseUrl = 'https://creditswagapi.herokuapp.com/';
-const baseUrl = 'https://8b6d0d95.ngrok.io/';
+const baseUrl = 'https://3165f7c5.ngrok.io/';
 
 export const loadData = state => ({
   type: LOAD_DATA,
   payload: state,
 });
 
-export const getSavingGoals = userId => async (dispatch) => {
-  const res = await axios.post(`${baseUrl}getSavingGoals`, { userId });
-  console.log('res.data:', res.data);
+export const getUser = () => async (dispatch) => {
   dispatch({
-    type: GET_SAVING_GOALS,
-    payload: res.data,
+    type: GET_USER,
+    payload: {
+      user: res.data.user,
+      goal: res.data.goal,
+    },
   });
 };
 
-export const addGoal = (goalData, userId) => async (dispatch) => {
+export const addGoal = (goalData, userId) => async (dispatch) => {  
   try {
     let res = await axios.post(`${baseUrl}addGoal`, { goalData, userId });    
-    console.log('res.data:', res.data);
     dispatch({
-      type: ADD_GOAL,
+      type: ADD_GOAL_UPDATE_USER,
+      payload: res.data,
+    });
+  } catch (err) {
+    dispatch({
+      type: HANDLE_ERROR,
+      payload: err
+    });
+  };
+};
+
+export const fetchGoal = (goal, userId, goalIndex) => async (dispatch) => {  
+  try {
+    let res = await axios.post(`${baseUrl}fetchGoal`, { goal, userId, goalIndex });    
+    dispatch({
+      type: FETCH_GOAL_UPDATE_USER,
       payload: res.data,
     });
   } catch (err) {
@@ -115,6 +129,29 @@ export const overdraftNotification = (notificationOnOrOff, userId) => async (dis
   };
 };
 
+// Public token isn't actually important it expires but we need to send it to getand savethe access token on User.
+export const connectBank = (userId, public_token) => async (dispatch) => {
+  try {
+    const res = await axios.post(`${baseUrl}get_access_token`, { public_token, userId });
+    if (res.status !== 200) {
+      dispatch({
+        type: HANDLE_ERROR,
+        payload: "Please check your connection or close the app and restart.",
+      });
+    } else { // Success.
+      dispatch({
+        type: CONNECT_BANK,
+        payload: public_token,
+      });
+    }
+  } catch (error) {  
+    dispatch({
+      type: HANDLE_ERROR,
+      payload: "Please check your connection or close the app and restart.",
+    }); 
+  }
+};
+
 export const getBalanceGraphData = userId => async (dispatch) => {
   const res = await axios.post(`${baseUrl}getBalanceGraphData`, { userId });
   dispatch({
@@ -123,45 +160,31 @@ export const getBalanceGraphData = userId => async (dispatch) => {
   });
 };
 
-export const connectBank = (userId, public_token) => async (dispatch) => {
-  await axios.post(`${baseUrl}get_access_token`, { public_token, userId });
-  dispatch({
-    type: CONNECT_BANK,
-    payload: public_token,
-  });
-};
-
 export const getTransactions = userId => async (dispatch) => {
+  console.log('userId:', userId);  
   const res = await axios.post(`${baseUrl}transactions`, { userId });
   dispatch({
-    type: GET_BALANCE_AND_TRANSACTIONS,
-    payload: res.data.user,
+    type: GET_BALANCE_AND_TRANSACTIONS,    
+    payload: res.data
   });
 };
 
-export function logIn(loginState) {
-  return (dispatcher) => {
-    try {
-      axios.post(`${baseUrl}login`, loginState).then((res) => {
-        dispatcher(handleLogin(res.data.user)); // THUNKED IT!
-      }).catch(() => {
-        dispatcher(handleError(true));
-      });
-    } catch (err) {
-      dispatcher(handleError(true));
-    }
-  };
+export const logIn = (loginState) => async (dispatch) => {
+  try {
+    res = await axios.post(`${baseUrl}login`, loginState);
+    dispatch({
+      type: HANDLE_LOGIN,
+      payload: res.data.user,
+    });
+  } catch (err) {
+    dispatch({
+      type: HANDLE_ERROR,
+      payload: true,
+    });
+  }
 };
 
-export const handleLogin = user => ({
-  type: HANDLE_LOGIN,
-  payload: user,
-  payload_error: false,
-});
-
-export function signUp(signupState, notificationToken) {
-  console.log('here');
-  
+export const signUp = (signupState, notificationToken) => {
   const headers = {
     'Content-Type': 'application/x-www-form-urlencoded',
     'Access-Control-Allow-Origin': '*',
@@ -169,7 +192,6 @@ export function signUp(signupState, notificationToken) {
   const data = {...signupState, notificationToken}
   return async (dispatcher) => {
     try {
-      console.log('2');      
       const res = await axios.post(`${baseUrl}signup`, data, headers);
       if (res.status === 200) {
         dispatcher(handleSignup(res.data));
@@ -182,12 +204,13 @@ export function signUp(signupState, notificationToken) {
   };
 };
 
-export const handleSignup = user => ({
+export const handleSignup = user => ({  
   type: HANDLE_SIGNUP,
   payload: user,
 });
 
 export const logOut = () => (dispatcher) => {
+  // axios.delete(`${baseUrl}logout`); No Need, I removed the cookies from ever even being set.
   dispatcher(handleLogout());
 };
 
@@ -200,3 +223,17 @@ export const handleError = error => ({
   type: HANDLE_ERROR,
   payload: error,
 });
+
+export const setError = () => async (dispatch) => {
+  dispatch({
+    type: SET_ERROR,    
+    payload: true
+  });
+};
+
+export const clearError = () => async (dispatch) => {  
+  dispatch({
+    type: CLEAR_ERROR,    
+    payload: false
+  });
+};
