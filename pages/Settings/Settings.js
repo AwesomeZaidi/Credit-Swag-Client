1d01b83e0bfcf5e6b2650f3176e63cddfc9f1b28
Asim Zaidi <asimzaidih@gmail.com>
2019-09-23 21:42:25 -0500

    temp fix:  hiding fontawesome for  android bug

--- pages/Settings/Settings.js
+++ pages/Settings/Settings.js
@@ -30,7 +30,7 @@ class Settings extends Component {
 
                 <View>
 
-                    <Text style={[common.text_label_gray, styles.pushOffDown, common.textLeft]}>Account</Text>
+                <Text style={[common.text_label_gray, styles.pushOffDown, common.textLeft]}>Account</Text>
                     
 
                     <TouchableHighlight style={styles.pushOffDownXl} onPress={() => this.props.navigation.navigate('Profile')}>
@@ -38,31 +38,34 @@ class Settings extends Component {
                             <Text style={[common.text_label_white, common.textLeft]}>
                                 Update Profile
                             </Text>
-                            <Text style={[common.text_label_white, common.textLeft]}>
+                            {/* <Text style={[common.text_label_white, common.textLeft]}>
                             <FontAwesomeIcon
                                 style={common.settingIcon}
                                 size={16}
                                 icon={ faArrowRight } 
                             />
-                            </Text>
+                            </Text> */}
 
                         </View>
                     </TouchableHighlight>
 
 
+
                     <Text style={[common.text_label_gray, styles.pushOffDown, common.textLeft]}>Alerts</Text>
+
+
                     <TouchableHighlight style={styles.pushOffDownXl} onPress={() => this.props.navigation.navigate('Notifications')}>
                         <View style={common.spaced_row_line}>
                             <Text style={[common.text_label_white, common.textLeft]}>
                                 Update Notifications
                             </Text>
-                            <Text style={[common.text_label_white, common.textLeft]}>
+                            {/* <Text style={[common.text_label_white, common.textLeft]}>
                             <FontAwesomeIcon
                                 style={common.settingIcon}
                                 size={16}
                                 icon={ faArrowRight } 
                             />
-                            </Text>
+                            </Text> */}
                         </View>                    
                     </TouchableHighlight>
                 </View>
